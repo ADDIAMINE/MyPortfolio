875845e7c680ae31e6b775808f57d256f34bc1ce
addi amine <amine.addi@gmail.com>
2020-07-06 12:48:29 +0200

    Ajoutez des fichiers projet.

---
+++ Core/Adress.cs
@@ -0,0 +1,9 @@
+﻿namespace Core
+{
+    public class Adress : EntityBase
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public int Number { get; set; }
+    }
+}
---
+++ Core/ProtfolioItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Core
+{
+   public class ProtfolioItem
+    {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
